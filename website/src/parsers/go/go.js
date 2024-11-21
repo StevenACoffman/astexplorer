94c6ea0c9d67acfec2f864d5b125ecee57e93665
Steve Coffman <steve@khanacademy.org>
2024-11-21 12:39:35 -0500

    Update for astexplorer-go
    Signed-off-by: Steve Coffman <steve@khanacademy.org>

--- website/src/parsers/go/go.js
+++ website/src/parsers/go/go.js
@@ -7,13 +7,13 @@ export default {
 
   id: ID,
   displayName: ID,
-  version: '1.13.4',
+  version: '1.23.3',
   homepage: 'https://golang.org/pkg/go/',
   _ignoredProperties: new Set(['_type']),
   locationProps: new Set(['Loc']),
 
   async loadParser(callback) {
-    require(['astexplorer-go'], async parser => {
+    require(['steve-astexplorer-go'], async parser => {
       await parser.init()
       callback(parser)
     })
